aabc7a9400caebe92996704c4f7fc9d5ec99aef8
Anton Cwik <antoncwik@Antons-MacBook-Pro.local>
2019-11-13 18:59:33 -0800

    fake server, procfile

--- bot.py
+++ bot.py
@@ -3,13 +3,14 @@ from checker import Checker
 from random import random
 import time
 import credentials
+from os import environ
+
+bot_token = environ.get('BOT_TOKEN') or credentials.BOT_TOKEN
+bot_chatID = environ.get('BOT_CHAT_ID') or credentials.BOT_CHAT_ID
 
 
 
 def telegram_bot_sendtext(bot_message):
-    
-    bot_token = credentials.BOT_TOKEN
-    bot_chatID = credentials.BOT_CHAT_ID
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
@@ -27,7 +28,8 @@ while True:
 		test = telegram_bot_sendtext("Found a website! Campground: " + result[1] + " Date: " + result[0])
 	else:
 		print 'Found Nothing'
-		test = telegram_bot_sendtext("Found Nothing")
+		#test = telegram_bot_sendtext("Found Nothing")
 
-	time.sleep(60 * 30 + 60 * 60 * random())
+	rand_number = random()
+	time.sleep(60 * 30 + (60 * 60 * rand_number))
 
